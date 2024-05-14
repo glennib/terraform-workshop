19c4b1db892c069592dfc976cf7a221f33564d31
Glenn Bitar <glennbitar@gmail.com>
2024-05-14 23:10:47 +0200

    .

--- scripts/pdf-to-svgs.sh
+++ scripts/pdf-to-svgs.sh
@@ -2,13 +2,15 @@
 
 # Convert a PDF to SVGs, one per page, suffixed by -1.svg, -2.svg, etc.
 
+set -e
+
 pdf=$1
 prefix=$(echo $pdf | sed 's/\.[^.]*$//')
-pages=$(pdfinfo "$pdf" | awk '/^Pages:/ {print $2}')
+n_pages=$(pdfinfo "$pdf" | awk '/^Pages:/ {print $2}')
 
-echo "Converting $pdf to $pages SVGs..." >&2
+echo "Converting $pdf to $n_pages SVGs..." >&2
 
-for (( page=1; page<=$pages; page++ ));
+for (( page=1; page<=$n_pages; page++ ));
 do
   destination="$prefix-$page.svg"
   pdf2svg "$pdf" "$destination" "$page"
